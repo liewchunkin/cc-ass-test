801c79db049868049578bfd6c68ee20b57c10515
liewchunkin <62841859+liewchunkin@users.noreply.github.com>
2022-09-20 17:56:21 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cqvsffcabzfa.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "liewchunkin-bucket"
+customregion = "us-east-1"
