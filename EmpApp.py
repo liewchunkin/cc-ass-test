78b2945abacfaf3a86cbf062a36a4dfb5060df7b
liewchunkin <62841859+liewchunkin@users.noreply.github.com>
2022-09-20 23:27:11 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -1,8 +1,10 @@
+from tkinter import E
 from flask import Flask, render_template, request
 from pymysql import connections
 import os
 import boto3
 from config import *
+from datetime import date
 
 app = Flask(__name__)
 
@@ -80,6 +82,45 @@ def AddEmp():
     print("all modification done...")
     return render_template('AddEmpOutput.html', name=emp_name)
 
+@app.route("/cin", methods=['POST'])
+def checkin():
+    try:
+        emp_id = request.form['emp_id']
+        selectSQL = "SELECT * FROM employee WHERE emp_id = "+emp_id
+        cursor = db_conn.cursor()
+        cursor.execute(selectSQL)
+        result = cursor.fetchall()
+        if(len(result)!=0):
+            querySQL = "INSERT INTO attendance(emp_id, check_in, check_out, payroll) VALUES(%s, %s, '', -1)"
+            cursor.execute(querySQL, (emp_id, date.today().strftime("%d-%m-%y")))
+            db_conn.commit()
+        else:
+            print ("<script>alert('No user found!!');</script>")
+    except Exception as e :
+        return str(e)
+    finally:
+        cursor.close()
+    return render_template('cin.html', empId=emp_id)
+
+@app.route("/cout", methods=['POST'])
+def checkout():
+    try:
+        emp_id = request.form['emp_id']
+        selectSQL = "SELECT * FROM employee WHERE emp_id = "+emp_id
+        cursor = db_conn.cursor()
+        cursor.execute(selectSQL)
+        result = cursor.fetchall()
+        if(len(result)!=0):
+            querySQL = "UPDATE attendance SET check_out = %s WHERE emp_id = %s"
+            cursor.execute(querySQL, (date.today().strftime("%d-%m-%y"), emp_id))
+            db_conn.commit()
+        else:
+            print ("<script>alert('No user found!!');</script>")
+    except Exception as e :
+        return str(e)
+    finally:
+        cursor.close()
+    return render_template('AttendanceEmp.html', empId=emp_id)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
